d48c089cd63b62a4b7da39c7545790cac8c28f8d
shyamsundar <reddyshyam3589@gmail.com>
2023-03-29 12:25:08 +0530

    test

--- terrafrom/practice/prometheus-server/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "ec2" {
-  ami                    = "ami-0089b8e98cd95257d"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0964abcbffb7f8bbf"]
-  tags = {
-    Name = "test"
-  }
-}
